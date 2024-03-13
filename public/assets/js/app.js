432e1a5744dd30e726b0f75cd64d6ac11d7a4a0f
HazimAdel101 <alsaqqafhazim2002@gmail.com>
2024-03-13 11:36:25 -0800

    Add svgs and clean the code

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,6 +4,18 @@ function toggleSubMenu(submenuId) {
     var collapsedIcon = document.getElementById(submenuId + 'CollapsedIcon');
     var expandedIcon = document.getElementById(submenuId + 'ExpandedIcon');
 
+    // Collapse all other submenus and reset their icons
+    var submenus = document.querySelectorAll('.submenu');
+    submenus.forEach(function(item) {
+        if (item.id !== submenuId + 'Submenu') {
+            item.style.display = 'none';
+            var subId = item.id.replace('Submenu', '');
+            document.getElementById(subId + 'CollapsedIcon').style.display = 'inline-block';
+            document.getElementById(subId + 'ExpandedIcon').style.display = 'none';
+        }
+    });
+
+    // Toggle the clicked submenu and update its icon
     if (submenu.style.display === 'block') {
         submenu.style.display = 'none';
         collapsedIcon.style.display = 'inline-block';
--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -3,9 +3,7 @@
     aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
 
-        <script>
-            
-        </script>
+        <script></script>
         <ul class="space-y-2 font-medium">
             <li class="">
                 <a onclick="toggleSubMenu('user')" href="#"
@@ -13,31 +11,20 @@
 
                     <div class="flex justify-between items-between w-full">
                         <div class="flex">
-                            <svg class="w-5 h-5  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-people-fill"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
-                            </svg>
-                            {{-- <img  class="w-5 h-5  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="{{asset('assets/images/user.svg')}}" alt=""> --}}
-
+                            <img class="w-5 h-5  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                src="{{ asset('assets/images/user.svg') }}" alt="">
                             <span class="ms-3">المستخدمين
                             </span>
                         </div>
                         <div class="flex items-center">
-                            <svg id="userCollapsedIcon"
+                            <img id="userCollapsedIcon"
                                 class="w-4 h-4 inline-block text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
-                                <path
-                                    d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                            </svg>
-                            <svg id="userExpandedIcon"
+                                src="{{ asset('assets/images/arrow-down.svg') }}" alt="">
+
+                            <img style="display: none;" id="userExpandedIcon"
                                 class="w-4 h-4 inline-block text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"
-                                style="display: none;">
-                                <path
-                                    d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-                            </svg>
+                                src="{{ asset('assets/images/arrow-up.svg') }}" alt="">
+
                         </div>
                     </div>
                 </a>
@@ -54,29 +41,22 @@
 
                     <div class="flex justify-between items-between w-full">
                         <div class="flex">
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
-                                <path
-                                    d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
-                            </svg>
+                            <img class="w-5 h-5  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                src="{{ asset('assets/images/level.svg') }}" alt="">
+
                             <span class="ms-3"> المستويات
                             </span>
                         </div>
                         <div class="flex items-center">
-                            <svg id="levelCollapsedIcon"
+
+                            <img id="levelCollapsedIcon"
                                 class="w-4 h-4 inline-block text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
-                                <path
-                                    d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                            </svg>
-                            <svg id="levelExpandedIcon"
+                                src="{{ asset('assets/images/arrow-down.svg') }}" alt="">
+
+                            <img style="display: none;" id="levelExpandedIcon"
                                 class="w-4 h-4 inline-block text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"
-                                style="display: none;">
-                                <path
-                                    d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-                            </svg>
+                                src="{{ asset('assets/images/arrow-up.svg') }}" alt="">
+
                         </div>
                     </div>
                 </a>
@@ -95,28 +75,20 @@
 
                     <div class="flex justify-between items-between w-full">
                         <div class="flex">
-                            <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                                <path
-                                    d="M192 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm-8 384V352h16V480c0 17.7 14.3 32 32 32s32-14.3 32-32V192h56 64 16c17.7 0 32-14.3 32-32s-14.3-32-32-32H384V64H576V256H384V224H320v48c0 26.5 21.5 48 48 48H592c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H368c-26.5 0-48 21.5-48 48v80H243.1 177.1c-33.7 0-64.9 17.7-82.3 46.6l-58.3 97c-9.1 15.1-4.2 34.8 10.9 43.9s34.8 4.2 43.9-10.9L120 256.9V480c0 17.7 14.3 32 32 32s32-14.3 32-32z" />
-                            </svg>
+                            <img class="w-5 h-5  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                src="{{ asset('assets/images/lesson.svg') }}" alt="">
                             <span class="ms-3"> الدروس
                             </span>
                         </div>
                         <div class="flex items-center">
-                            <svg id="lessonCollapsedIcon"
+                            <img id="lessonCollapsedIcon"
                                 class="w-4 h-4 inline-block text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
-                                <path
-                                    d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                            </svg>
-                            <svg id="lessonExpandedIcon"
+                                src="{{ asset('assets/images/arrow-down.svg') }}" alt="">
+
+                            <img style="display: none;" id="lessonExpandedIcon"
                                 class="w-4 h-4 inline-block text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"
-                                style="display: none;">
-                                <path
-                                    d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-                            </svg>
+                                src="{{ asset('assets/images/arrow-up.svg') }}" alt="">
+
                         </div>
                     </div>
                 </a>
@@ -135,28 +107,21 @@
 
                     <div class="flex justify-between items-between w-full">
                         <div class="flex">
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor">
-                                <path
-                                    d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
-                            </svg>
+                            <img class="w-5 h-5  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                src="{{ asset('assets/images/quiz.svg') }}" alt="">
+
                             <span class="ms-3"> الاختبارات
                             </span>
                         </div>
                         <div class="flex items-center">
-                            <svg id="quizCollapsedIcon"
+                            <img id="quizCollapsedIcon"
                                 class="w-4 h-4 inline-block text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
-                                <path
-                                    d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                            </svg>
-                            <svg id="quizExpandedIcon"
+                                src="{{ asset('assets/images/arrow-down.svg') }}" alt="">
+
+                            <img style="display: none;" id="quizExpandedIcon"
                                 class="w-4 h-4 inline-block text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"
-                                style="display: none;">
-                                <path
-                                    d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-                            </svg>
+                                src="{{ asset('assets/images/arrow-up.svg') }}" alt="">
+
                         </div>
                     </div>
                 </a>
